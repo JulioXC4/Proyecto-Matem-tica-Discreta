ec8a6d16abb7d339f40f50d0bb13a78395394d29
JulioXC4 <danielc4nt@gmail.com>
2024-05-11 14:44:50 -0500

    Page ready for deployment in vercel

--- app/components/Tree/components/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as InputTreeData } from './InputTreeData';
-export { default as Modal } from './Modal';
-export { default as Table } from './Table';
-//export { default as TreeDisplay } from './TreeDisplay';
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import LandingPage from "./components/LandingPage/LandingPage";
-import Tree from "./components/Tree";
 
 export default function Home() {
   return <LandingPage />;
